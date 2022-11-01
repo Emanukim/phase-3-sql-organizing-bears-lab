843222d72ff8abff2792e9b0483f45f190fc604e
Emanukim <emmanue.kimweli@student.moringaschool.com>
2022-11-01 12:19:22 +0300

    Completed lab

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+-- INSERT INTO bears ( name) VALUES ("Mr. Chocolate");
+-- INSERT INTO bears ( name) VALUES ("Rowdy");
+-- INSERT INTO bears ( name) VALUES ("Tabitha");
+-- INSERT INTO bears ( name) VALUES ("Sergeant Brown");
+-- INSERT INTO bears ( name) VALUES ("Melissa");
+-- INSERT INTO bears ( name) VALUES ("Grinch");
+-- INSERT INTO bears ( name) VALUES ("Wendy");
+-- INSERT INTO bears ( name) VALUES (NULL);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+("Mr. Chocolate", 20, "M", "dark brown", "calm", 0),
+("Rowdy", 10, "M", "black", "intense", 1),
+("Tabitha", 6, "F", "dark brown", "Nice", 1),
+("Sargeant Brown", 19, "M", "Green", "Slimy", 0),
+("Melissa", 13, "F", "dark brown", "goofy", 1),
+("Grinch", 2, "M", "Black", "Grinchy", 1),
+("Wendy", 6, "F", "Blue", "naive", 1),
+(null, 20, "M", "black", "aggressive", 0);
+
+
+
+
+
